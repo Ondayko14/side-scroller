998d58b89027c39ebf9e197d0728dd56a4f86fb2
Brandon O <b.ondayko@gmail.com>
2020-12-03 15:42:47 -0600

    Rectangles are now clear and randomly generating a color

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,4 +1,41 @@
 var canvas = document.getElementById("game-area");
 var ctx = canvas.getContext('2d');
+ctx.translate(0.5, 0.5);
 
-console.log(ctx.canvas);
+
+//randomly sized rectangle generator
+var rectangle = function () {
+    randomY = Math.floor(Math.random() * canvas.height);
+    randomW = Math.floor(Math.random() * canvas.width);
+    randomH = Math.floor(Math.random() * canvas.height);
+
+    ctx.fillStyle = getColor();
+    ctx.fillRect(canvas.width-100, randomY, randomW, randomH);
+};
+
+//Random Color for fill style
+var getColor = function () {
+    var color0 = "#E9C46A";
+    var color1 = "#F4A261";
+    var color2 = "#264653";
+    var color3 = "#E76F51";
+
+    var randomColorSelector = Math.abs(Math.floor(Math.random() * 4));
+
+    switch (randomColorSelector) {
+        case 0:
+            return color0;
+            break;
+        case 1:
+            return color1;
+            break;
+        case 2:
+            return color2;
+            break;
+        case 3:
+            return color3;
+            break;
+    }
+};
+
+rectangle();
